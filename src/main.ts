7a674061a8fbfa31d68e65fe7f9688405a0a6674
taishi1116 <shinoda1116.miwa@gmail.com>
2022-10-04 21:42:08 +0900

    feat: add constant.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PORT } from './util/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
